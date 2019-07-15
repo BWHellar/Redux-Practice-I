1d2b05eb6f042d82e33943b6321af39f5700d5d8
bh <bwhellar@gmail.com>
2019-07-15 09:36:49 -0700

    initial commit with json files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import rootReducer from './reducers';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer);
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
+
