35c608a43b2fedc58c0c3099b748ed8bbccd672b
bh <bwhellar@gmail.com>
2019-07-15 14:09:16 -0700

    background and styling

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import { addCharacterById } from '../actions';
 
 class CharacterList extends Component {
@@ -9,15 +9,15 @@ class CharacterList extends Component {
     return (
       <div>
         <h4>Characters</h4>
-        <ul>
+        <ul className="list-group">
           {
             this.props.characters.map(character => {
               return (
-                <li key={character.id}>
-                  <div>{character.name}</div>
-                  <div onClick={() => this.props.addCharacterById(character.id)}>
+                <li key={character.id} className="list-group-item">
+                  <div className="list-item">{character.name}</div>
+                  <div className="list-item right-button" onClick={() => this.props.addCharacterById(character.id)}>
                   
-                    +
+                    &#x2694;
                     </div>
                 </li>
               )
@@ -34,9 +34,9 @@ class CharacterList extends Component {
     };
   }
   
-  function mapDispatchToProps(dispatch){
-    return bindActionCreators({ addCharacterById}, dispatch)
-  }
+  // function mapDispatchToProps(dispatch){
+  //   return bindActionCreators({ addCharacterById}, dispatch)
+  // }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
+export default connect(mapStateToProps, {addCharacterById})(CharacterList);
